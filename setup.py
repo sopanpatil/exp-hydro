b6e223e566187861eea2f9c37170fb7a7f248543
sopanpatil <sopanpatil@protonmail.com>
2016-06-17 22:42:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
 setup(name='exphydro',
-	version='1.0',
-	description='EXP-HYDRO hydrological model',
-	author='Sopan Patil and Marc Stieglitz',
-	license='MIT',
-	packages=['exphydro'],
-	install_requires=['numpy','scipy','matplotlib'],
-	classifiers=[
-		'Programming Language :: Python :: 2.7',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Scientific/Engineering',
-		'Intended Audience :: Science/Research'
-		],
-	)
-
+      version='1.1',
+      description='EXP-HYDRO hydrological model',
+      author='Sopan Patil and Marc Stieglitz',
+      license='MIT',
+      packages=['exphydro'],
+      install_requires=['numpy', 'scipy', 'matplotlib'],
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Scientific/Engineering',
+          'Intended Audience :: Science/Research'
+      ],
+      )
